83da432a20fab92d205c6005f42ce653e5b601ef
Seth Lang <sethwlang@protonmail.com>
2022-08-20 20:48:58 -0400

    first deploy

---
+++ routes/index.tsx
@@ -0,0 +1,14 @@
+/** @jsx h */
+import { h } from "preact";
+
+
+export default function Home() {
+  return (
+    <main>
+    <div>
+      Home
+    </div>
+    <div><a href="/pocket">Pocket</a> | <a href="/spells">Spells</a></div>
+    </main>
+  );
+}
---
+++ routes/spells.tsx
@@ -0,0 +1,14 @@
+/** @jsx h */
+import { h } from "preact";
+
+
+export default function Spells() {
+  return (
+    <main>
+    <div>
+      Spells
+    </div>
+    <div><a href="/">Home</a> | <a href="/pocket">Pocket</a></div>
+    </main>
+  );
+}
