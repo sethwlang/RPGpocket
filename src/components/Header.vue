e86aa14296289608e4a10482a22a1d661455238e
Seth Lang <sethwlang@protonmail.com>
2022-08-21 20:23:02 -0400

    inventory grab WIP

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav>
+        <router-link to="/" >Home</router-link> | 
+        <router-link to="/pocket" >Pocket</router-link> | 
+        <router-link to="/spells" >Spells</router-link>
+    </nav>
+</template>
