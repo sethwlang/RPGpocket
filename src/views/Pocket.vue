7e7200d6ec0994fbfa08459dfaa59f5fb662f9a7
Seth Lang <sethwlang@protonmail.com>
2022-08-27 17:37:33 -0400

    Add from store to inventory WIP
    with total weight calc

--- src/views/Pocket.vue
+++ src/views/Pocket.vue
@@ -7,15 +7,54 @@
         <option value="dnd" >Dungeons and Dragons</option>
         <option value="deadlands" >Deadlands</option>
     </select>
-    <div class="store"></div>
-    <div class="pocket"></div>
+    <div class="store" v-show="storeInventory" style="width:40%">
+      <h3>Store</h3>
+      <div>Search: <input/></div>
+      <table style="border: solid">
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Weight</th>
+        <th>Type</th>
+        <th>Add</th>
+        <tr v-for="item in storeInventory" :key="item.id">
+          <td>{{item.name}}</td>
+          <td>{{item.description}}</td>
+          <td>{{item.price}}</td>
+          <td>{{item.weight}}</td>
+          <td>{{item.type}}</td>
+          <td><button @click.prevent="addToInventory(item)">Add</button></td>
+        </tr>
+      </table>
+    </div>
+    <div class="pocket" v-show="myInventory.length >0" style="width:40%">
+      <h3>Pocket</h3><span>Total Weight:<span>{{totalWeight}}</span></span>
+      <table style="border: solid">
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Weight</th>
+        <th>QTY</th>
+        <tr v-for="item in myInventory" :key="item.id">
+          <td>{{item.name}}</td>
+          <td>{{item.description}}</td>
+          <td>{{item.price}}</td>
+          <td>{{item.weight}}</td>
+          <td>{{item.qty}}</td>
+        </tr>
+      </table>
+    </div>
   </main>
 </template>
 <script>
+
 export default {
+  name:"RPGpocket",
   data() {
     return {
       storeInventory:0,
+      myInventory:[],
+      totalWeight:0,
     }
   },
   mounted() {
@@ -29,7 +68,20 @@ export default {
                 .then((response) => response.json())
                 .then((json) => this.storeInventory = json);
             }
-        }
+        },
+        addToInventory(item){
+          item.qty = 1;
+          //for lopp to check if item has already been added or not.
+            this.myInventory.push(item);
+            this.totalWeight += item.weight;
+
+        },
+        search(searchValue){
+
+            
+
+        },
+    
 
   },
 }
